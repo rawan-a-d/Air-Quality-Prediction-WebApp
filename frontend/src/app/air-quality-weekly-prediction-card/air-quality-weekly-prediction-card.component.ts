f7c4fced489aef94089dd7663e0709f2961214b3
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2022-12-23 20:32:43 +0100

    Refactor and clean up

--- frontend/src/app/air-quality-weekly-prediction-card/air-quality-weekly-prediction-card.component.ts
+++ frontend/src/app/air-quality-weekly-prediction-card/air-quality-weekly-prediction-card.component.ts
@@ -14,13 +14,7 @@ import { WeatherService } from "../services/weather.service";
 import { AirQualityPredictionDialog } from "../air-quality-prediction-dialog/air-quality-prediction-dialog.component";
 import { AirQualityPrediction } from "../models/AirQualityPrediction";
 import { NgForm } from "@angular/forms";
-
-export interface DialogData {
-	airQualityLevel: "Good" | "Moderate" | "Unhealthy for Sensitive Groups" | "Unhealthy" | "Very Unhealthy" | "Hazardous";
-	airQualityLevelNumerical: 0,
-	zipcode: "";
-	date: "";
-}
+import { ZipCodes } from "../models/ZipCodes";
 
 @Injectable()
 export class FiveDayRangeSelectionStrategy implements MatDateRangeSelectionStrategy<string> {
@@ -63,45 +57,15 @@ export class FiveDayRangeSelectionStrategy implements MatDateRangeSelectionStrat
 export class AirQualityWeeklyPredictionCardComponent {
 	showSpinner = false;
 
-	zipcodes: ZipCode[] = [
-		{ value: "5611", viewValue: "5611" },
-		{ value: "5612", viewValue: "5612" },
-		{ value: "5613", viewValue: "5613" },
-		{ value: "5614", viewValue: "5614" },
-		{ value: "5615", viewValue: "5615" },
-		{ value: "5616", viewValue: "5616" },
-		{ value: "5617", viewValue: "5617" },
-		{ value: "5621", viewValue: "5621" },
-		{ value: "5622", viewValue: "5622" },
-		{ value: "5623", viewValue: "5623" },
-		{ value: "5624", viewValue: "5624" },
-		{ value: "5625", viewValue: "5625" },
-		{ value: "5626", viewValue: "5626" },
-		{ value: "5627", viewValue: "5627" },
-		{ value: "5628", viewValue: "5628" },
-		{ value: "5629", viewValue: "5629" },
-		{ value: "5631", viewValue: "5631" },
-		{ value: "5632", viewValue: "5632" },
-		{ value: "5633", viewValue: "5633" },
-		{ value: "5641", viewValue: "5641" },
-		{ value: "5642", viewValue: "5642" },
-		{ value: "5643", viewValue: "5643" },
-		{ value: "5644", viewValue: "5644" },
-		{ value: "5645", viewValue: "5645" },
-		{ value: "5646", viewValue: "5646" },
-		{ value: "5651", viewValue: "5651" },
-		{ value: "5652", viewValue: "5652" },
-		{ value: "5653", viewValue: "5653" },
-		{ value: "5654", viewValue: "5654" },
-		{ value: "5655", viewValue: "5655" },
-		{ value: "5656", viewValue: "5656" },
-		{ value: "5657", viewValue: "5657" },
-		{ value: "5658", viewValue: "5658" },
-	];
+	zipcodes: ZipCode[] = ZipCodes.zipCodes;
 
 	peopleNumber: number = 216; //hard coded
 	boundaryLayerHeight: number = 629.35; //hard coded
 	model = new AirQuality(new Date(), "5611", this.peopleNumber, new MeteoData(0, 0, 0, this.boundaryLayerHeight));
+
+	// start and end dates
+	start: string;
+	end: string;
 
 	constructor(public dialog: MatDialog,
 		private appService: AppService,
@@ -115,27 +79,35 @@ export class AirQualityWeeklyPredictionCardComponent {
 
 	// load weekly weather data based on start and end date range
 	loadWeeklyWeatherData(startDate?, endDate?) {
-		var start: string;
-		var end: string;
-
 		// if dates are selected
 		if (startDate && endDate) {
-			start = this.datePipe.transform(startDate.value, "yyyy-MM-dd");
-			end = this.datePipe.transform(endDate.value, "yyyy-MM-dd");
+			this.start = startDate.value;
+			this.end = endDate.value;
 		}
-		// use current data
+		// use current date
 		else {
 			var fiveDayRangeSelectionStrategy = new FiveDayRangeSelectionStrategy();
 
-			var currentDate = this.datePipe.transform(new Date(), "yyyy-MM-dd");
-			var dateRange = fiveDayRangeSelectionStrategy.createPreview(currentDate);
+			startDate = new Date();
+			var startDateFormatted = this.datePipe.transform(startDate, "yyyy-MM-dd");
+			var dateRange = fiveDayRangeSelectionStrategy.createPreview(startDateFormatted);
 
-			start = this.datePipe.transform(dateRange.start, "yyyy-MM-dd");
-			end = this.datePipe.transform(dateRange.end, "yyyy-MM-dd");
+			this.start = dateRange.start;
+			this.end = dateRange.end;
 		}
 
+		var startFormatted = this.datePipe.transform(this.start, "yyyy-MM-dd");
+		var endFormatted = this.datePipe.transform(this.end, "yyyy-MM-dd");
+
+		// update model
+		this.model.date = startDate.value || startDate;
+
+		this.getWeeklyWeatherData(startFormatted, endFormatted);
+	}
+
+	getWeeklyWeatherData(startFormatted, endFormatted) {
 		// get weather data
-		this.weatherService.LoadWeatherAPI(start, end).subscribe(
+		this.weatherService.LoadWeatherAPI(startFormatted, endFormatted).subscribe(
 			res => {
 				const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
 
@@ -145,25 +117,28 @@ export class AirQualityWeeklyPredictionCardComponent {
 					this.boundaryLayerHeight);
 				this.model.meteoData = meteoData;
 			})
-
 	}
 
 	openDialog(airQualityLevel: string, airQualityLevelNumerical: number) {
 		// get dialog class name
 		var className = airQualityLevel.split(" ").join("-").toLowerCase();
 
+		var formattedStartDate: string | null = this.datePipe.transform(this.start, "mediumDate")
+		var formattedEndDate: string | null = this.datePipe.transform(this.end, "mediumDate")
+
 		this.dialog.open(AirQualityPredictionDialog, {
 			data: {
 				airQualityLevel: airQualityLevel,
 				airQualityLevelNumerical: airQualityLevelNumerical,
 				zipcode: this.model.zipcode,
-				date: this.model.date,
+				period: `between ${formattedStartDate} and ${formattedEndDate}`
 			},
 			panelClass: className,
 		});
 	}
 
 	getPrediction(form: NgForm) {
+		// format date
 		var formattedDate: string | null = this.datePipe.transform(this.model.date, "yyyy-MM-dd");
 
 		// show spinner
@@ -174,6 +149,7 @@ export class AirQualityWeeklyPredictionCardComponent {
 			.subscribe({
 				next: (data: AirQualityPrediction) => {
 					console.log(data);
+
 					// open dialog
 					this.openDialog(data.airQualityLevel, data.airQualityLevelNumerical);
 				},
---
+++ frontend/src/app/models/DialogData.ts
@@ -0,0 +1,6 @@
+export interface DialogData {
+	airQualityLevel: "Good" | "Moderate" | "Unhealthy for Sensitive Groups" | "Unhealthy" | "Very Unhealthy" | "Hazardous";
+	airQualityLevelNumerical: 0,
+	zipcode: "";
+	period: "";
+}
